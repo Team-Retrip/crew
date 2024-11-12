0b8e43f05b29728da43134c92b0c2a61f636e313
mandykr <hongseokii92@gmail.com>
2024-11-12 13:02:45 +0900

    feat: add create Crew

--- src/main/java/com/retrip/crew/application/CrewService.java
+++ src/main/java/com/retrip/crew/application/CrewService.java
@@ -1,7 +1,12 @@
 package com.retrip.crew.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class CrewService {
+    public void create() {
+
+    }
 }
