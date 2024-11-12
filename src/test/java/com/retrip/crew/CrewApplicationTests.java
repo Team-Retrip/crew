098a9a619f95616f346d9c51ca89601d7e2304f3
mandykr <hongseokii92@gmail.com>
2024-11-12 11:58:29 +0900

    initial commit

---
+++ src/test/java/com/retrip/crew/CrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrip.crew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
