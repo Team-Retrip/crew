7216ab5be566465447b24d9274563f5371c84adc
mandykr <hongseokii92@gmail.com>
2024-11-12 12:41:04 +0900

    feat: add CrewMember ID field

--- src/main/java/com/retrip/crew/domain/CrewMember.java
+++ src/main/java/com/retrip/crew/domain/CrewMember.java
@@ -1,5 +1,13 @@
 package com.retrip.crew.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
 public class CrewMember {
+    @Id
+    private UUID id;
     private String name;
 }
