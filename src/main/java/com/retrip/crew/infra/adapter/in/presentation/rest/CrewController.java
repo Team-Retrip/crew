cf2b6c467b07d8c76c9c78df1fda8979e23f6053
junhokim <junhokim@gmarket.com>
2025-02-26 09:43:04 +0900

    feat: 스웨거 추가 작업

--- src/main/java/com/retrip/crew/infra/adapter/in/presentation/rest/CrewController.java
+++ src/main/java/com/retrip/crew/infra/adapter/in/presentation/rest/CrewController.java
@@ -3,6 +3,9 @@ package com.retrip.crew.infra.adapter.in.presentation.rest;
 import com.retrip.crew.application.in.request.CrewCreateRequest;
 import com.retrip.crew.application.in.response.CrewCreateResponse;
 import com.retrip.crew.application.in.usecase.CreateCrewUseCase;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperties;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +18,12 @@ import java.net.URI;
 @RequiredArgsConstructor
 @RequestMapping("/crews")
 @RestController
+@Tag(name = "Crew", description = "크루 서비스")
 public class CrewController {
     private final CreateCrewUseCase createCrewUseCase;
 
     @PostMapping
+    @Schema(description = "크루 생성")
     public ResponseEntity<CrewCreateResponse> createCrew(@RequestBody CrewCreateRequest request) {
         CrewCreateResponse crew = createCrewUseCase.createCrew(request);
         return ResponseEntity.created(URI.create("/crews/" + crew.id())).body(crew);
