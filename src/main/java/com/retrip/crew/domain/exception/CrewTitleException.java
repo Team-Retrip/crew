8c87433e70e37158c56de1310b82c8ed4777e5c7
mandykr <94955454+mandykr@users.noreply.github.com>
2025-02-27 10:37:20 +0900

    feat: 비즈니스 예외와 API 공통 응답 모듈 추가 (#23)
    * feat: 비즈니스 예외와 API 공통 응답 모듈 추가
    - vo 검증에서 발생하는 비즈니스 예외 삭제 및 InvalidValueException 으로 변경

--- src/main/java/com/retrip/crew/domain/exception/CrewTitleException.java
+++
@@ -1,7 +0,0 @@
-package com.retrip.crew.domain.exception;
-
-public class CrewTitleException extends CrewException {
-    public CrewTitleException(String message) {
-        super(CrewErrorCode.CREW_TITLE_ERROR, message);
-    }
-}
--- src/main/java/com/retrip/crew/domain/vo/PostContent.java
+++ src/main/java/com/retrip/crew/domain/vo/PostContent.java
@@ -1,6 +1,6 @@
 package com.retrip.crew.domain.vo;
 
-import com.retrip.crew.domain.exception.CrewDescriptionException;
+import com.retrip.crew.domain.exception.common.InvalidValueException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
@@ -25,7 +25,7 @@ public class PostContent {
 
     private void validate(String value) {
         if (value.length() > CONTENT_LENGTH_LIMIT) {
-            throw new CrewDescriptionException("자유 게시판 내용은 " + CONTENT_LENGTH_LIMIT + "자를 넘을 수 없습니다.");
+            throw new InvalidValueException("자유 게시판 내용은 " + CONTENT_LENGTH_LIMIT + "자를 넘을 수 없습니다.");
         }
     }
 }
