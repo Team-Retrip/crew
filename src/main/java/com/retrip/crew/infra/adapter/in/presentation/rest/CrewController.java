5bb16e8546d7d30241b9e161929464259b6c042d
Kimjunho <39546306+xjvmdutl@users.noreply.github.com>
2025-03-03 18:06:21 +0900

    feat: 스웨거 추가 작업 (#24)
    Co-authored-by: junhokim <junhokim@gmarket.com>

--- src/main/java/com/retrip/crew/infra/adapter/in/presentation/rest/CrewController.java
+++ src/main/java/com/retrip/crew/infra/adapter/in/presentation/rest/CrewController.java
@@ -3,15 +3,9 @@ package com.retrip.crew.infra.adapter.in.presentation.rest;
 import com.retrip.crew.application.in.request.CrewCreateRequest;
 import com.retrip.crew.application.in.response.CrewCreateResponse;
 import com.retrip.crew.application.in.usecase.CreateCrewUseCase;
-import com.retrip.crew.domain.exception.CrewNotFoundException;
-import com.retrip.crew.domain.exception.common.ErrorCode;
-import com.retrip.crew.domain.exception.common.InvalidValueException;
-import com.retrip.crew.infra.adapter.in.presentation.rest.common.ApiResponse;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,10 +14,12 @@ import java.net.URI;
 @RequiredArgsConstructor
 @RequestMapping("/crews")
 @RestController
+@Tag(name = "Crew", description = "크루 서비스")
 public class CrewController {
     private final CreateCrewUseCase createCrewUseCase;
 
     @PostMapping
+    @Schema(description = "크루 생성")
     public ResponseEntity<CrewCreateResponse> createCrew(@RequestBody CrewCreateRequest request) {
         CrewCreateResponse crew = createCrewUseCase.createCrew(request);
         return ResponseEntity.created(URI.create("/crews/" + crew.id())).body(crew);
