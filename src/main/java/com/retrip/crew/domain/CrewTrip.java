468262dcf90bc16a16d220f1b43f48c1ab75376a
mandykr <hongseokii92@gmail.com>
2025-04-27 16:44:04 +0900

    chore: 오타 수정

--- src/main/java/com/retrip/crew/domain/CrewTrip.java
+++ src/main/java/com/retrip/crew/domain/CrewTrip.java
@@ -1,6 +1,5 @@
 package com.retrip.crew.domain;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,12 +7,8 @@ import java.util.UUID;
 
 import static com.retrip.crew.domain.CrewTrip.CrewTripType.EXCLUSION;
 
-@Schema(description = "크루 여행")
 public record CrewTrip(
-        @Schema(description = "크루 ID")
         UUID tripId,
-
-        @Schema(description = "크루 여행 타입")
         CrewTripType type
 ) {
     public boolean isImpossibleWithdrawal() {
